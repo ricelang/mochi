2c1168136c04eae54ef674df0f5802bff39ead6c
Vinh <kurei@axcoto.com>
2016-04-01 10:22:11 -0700

    Move footprint to its own

--- cmd/mochi/main.go
+++ cmd/mochi/main.go
@@ -15,8 +15,12 @@ const (
 	Lang = "Mochi"
 )
 
-func main() {
+func footprint() {
 	fmt.Printf("\033[48;5;95;38;5;214m%s. Rev %s\033[0m\n", Lang, VERSION)
+}
+
+func main() {
+	footprint()
 
 	if len(os.Args) > 1 {
 		interpreter.Run(os.Args[1])
